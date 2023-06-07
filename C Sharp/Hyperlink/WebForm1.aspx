17b3821b92b2a340864423873a081ae063b0d6ed
Rupali-Barde <rupalibarde311@gmail.com>
2023-06-07 17:07:53 +0530

    File Added

---
+++ C Sharp/Hyperlink/WebForm1.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Hyperlink.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-left: 32px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            Cookie Example<br />
+            <br />
+            Username&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Email&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server" CssClass="auto-style1" Width="174px"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Store in temprory cookie" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="View Temprory Cookie" />
+            <br />
+            <br />
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Store in Permanent Cookie" />
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="View in permanent Cookie" />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ C Sharp/Hyperlink/WebForm1.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hyperlink
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("cook1");
+            cookie.Values.Add("Username", TextBox1.Text);
+            Response.Cookies.Add(cookie);
+            Label1.Text = "Temprory Cookie Added To Client Machine";
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie = Request.Cookies["cook1"];
+            if (cookie != null) 
+            {
+                string uname = cookie.Values["Username"].ToString();
+                Label1.Text = "Username" + uname;
+
+            }
+            else
+            {
+                Label1.Text = "Cookie Does Not Exist";
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("cook2");
+            cookie.Values.Add("Username",  TextBox1.Text);
+            cookie.Values.Add("Email",  TextBox2.Text);
+            DateTime dt = DateTime.Now;
+            TimeSpan ts = new TimeSpan(0, 1, 0);
+            cookie.Expires = dt.Add(ts);
+            Label1.Text = "Permanent Cookie Added To Client Machine";
+            Response.Cookies.Add(cookie);
+
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie = Request.Cookies["cook1"];
+            if (cookie != null) 
+            {
+                string uname = cookie.Values["Username"].ToString();
+                string email = cookie.Values["Email"].ToString();
+                Label1.Text = "Username" + uname;
+                Label2.Text = "Email" + email;
+            }
+            else
+            {
+                Label1.Text = "No Permanent Cookie Exist";
+            }
+        }
+    }
+}
