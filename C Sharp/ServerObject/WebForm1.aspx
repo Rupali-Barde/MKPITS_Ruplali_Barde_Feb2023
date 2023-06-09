88aa19c2bf3d741a2f4bf4637c73780ba0011779
Rupali-Barde <rupalibarde311@gmail.com>
2023-06-09 18:14:09 +0530

    File Added

---
+++ C Sharp/ServerObject/WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ServerObject.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Web Form1 Page<br />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Execute Method" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Transfer Method" />
+            <br />
+            <br />
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Html Encode Server" />
+&nbsp;
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="URL Encode" />
+        </div>
+        <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="URL Method 2" />
+&nbsp;
+        <asp:Button ID="Button6" runat="server" OnClick="Button6_Click" Text="URL Method 3" />
+&nbsp;<br />
+        <br />
+        <asp:Button ID="Button7" runat="server" OnClick="Button7_Click" Text="MapPath Method" />
+    </form>
+</body>
+</html>
---
+++ C Sharp/ServerObject/WebForm1.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerObject
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("Hello From Webpage1 Form");
+            Server.Execute("webform2.aspx");
+            Response.Write("Welcome Back To WebPage1");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Write("Hello From Webpage1 Transfer Form");
+            Server.Transfer("webform2.aspx");
+            Response.Write("Welcome Back To WebPage1");
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Write(Server.HtmlEncode("<h1> is an example of a Heading Tag</h1>"));
+            Response.Write("<h1> Is An Example Of A Heading Tag");
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Response.Write(Server.UrlEncode("http://localhost/code/map.aspx"));
+            Response.Write("http://localhost/code/map.aspx");
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            string name = Server.UrlEncode("Rupali Barde");
+            Response.Redirect("webform2.aspx?name=" +name);
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+
+            string name = Server.UrlEncode("Rupali Barde");
+
+            string password = Server.UrlEncode("queen");
+
+            Response.Redirect("Webform3.aspx?name=" + name + "&password=" + password);
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            Response.Write(Server.MapPath("/webform3.aspx"));
+        }
+    }
+}
