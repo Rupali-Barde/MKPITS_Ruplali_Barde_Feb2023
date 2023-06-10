c8ea78e985ef7a85e39c4fb138c38ecd6f27e1b3
Rupali-Barde <rupalibarde311@gmail.com>
2023-06-10 16:15:19 +0530

    File Added

---
+++ C Sharp/ModalForm1/SignUp.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ModalForm1
+{
+    public partial class SignUp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string email, psw;
+            email = Request.QueryString["email"].ToString();
+            psw = Request.QueryString["psw"].ToString();
+
+            Response.Write("email :" + email +"<br>");
+            Response.Write("Password:"+psw +"<br>");
+
+        }
+    }
+}
