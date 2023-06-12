97bba4c21d78a255069c19b518c727bd5768541e
Rupali-Barde <rupalibarde311@gmail.com>
2023-06-12 14:01:16 +0530

    File Added

---
+++ C Sharp/ADOEx1/WebForm1ADO.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1ADO.aspx.cs" Inherits="ADOEx1.WebForm1ADO" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    &nbsp;<br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click1" Text="Load" />
+        <br />
+    </form>
+</body>
+</html>
---
+++ C Sharp/ADOEx1/WebForm1ADO.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Security;
+
+namespace ADOEx1
+{
+    public partial class WebForm1ADO : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable("dTable");
+            DataRow dr;
+            dt.Columns.Add(new DataColumn("ID", typeof(int)));
+            dt.Columns.Add(new DataColumn("Name", typeof(string)));
+            dt.Columns.Add(new DataColumn("DOA", typeof(DateTime)));
+            dr = dt.NewRow();
+            dr[0] = 1;
+            dr[1] = "Rupali";
+            dr[2] = DateTime.Now;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[0] = 2;
+            dr[1] = "Mayuri";
+            dr[2] = DateTime.Now;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[0] = 3;
+            dr[1] = "Mahima";
+            dr[2] = DateTime.Now;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[0] = 4;
+            dr[1] = "Shreya";
+            dr[2] = DateTime.Now;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[0] = 5;
+            dr[1] = "Akanksha";
+            dr[2] = DateTime.Now;
+            dt.Rows.Add(dr);
+
+            ds.Tables.Add(dt);
+            GridView1.DataSource = ds.Tables["dTable"].DefaultView;
+            GridView1.DataBind();
+        }
+    }
+}
