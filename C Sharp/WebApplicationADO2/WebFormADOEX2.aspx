a541a683f2e75a4f058103aaef83822894b58bb7
Rupali-Barde <rupalibarde311@gmail.com>
2023-06-12 15:53:06 +0530

    File Added

---
+++ C Sharp/WebApplicationADO2/WebFormADOEX2.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormADOEX2.aspx.cs" Inherits="WebApplicationADO2.WebFormADOEX2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <p>
+            Total Amount&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Amount Paid
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:RadioButton ID="RadioButton1" runat="server" OnCheckedChanged="RadioButton1_CheckedChanged" Text="Cash" />
+&nbsp;
+            <asp:RadioButton ID="RadioButton2" runat="server" Text="EMI" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Show EMI" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ C Sharp/WebApplicationADO2/WebFormADOEX2.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+namespace WebApplicationADO2
+{
+    public partial class WebFormADOEX2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int tamount = Convert.ToInt32(TextBox1.Text);
+            int pamount = Convert.ToInt32(TextBox2.Text);
+
+            int ramount = tamount - pamount;
+            double eamount = 0;
+
+            if (RadioButton1.Checked)
+            {
+                if (ramount > 0)
+                {
+                    Label1.Text = "please pay Full Amount In Cash";
+
+
+                }
+                else if (RadioButton2.Checked)
+                {
+                    if (ramount > 0)
+                    {
+                        eamount = ramount / 3;
+
+                    }
+                }
+                string pname = "mouse";
+                DataSet ds = new DataSet();
+                DataTable dt = new DataTable("emitable");
+                DataRow dr;
+                dt.Columns.Add(new DataColumn("srno", typeof(int)));
+                dt.Columns.Add(new DataColumn("productname", typeof(string)));
+                dt.Columns.Add(new DataColumn("emi amount", typeof(decimal)));
+                for (int i = 1; i <= 3; i++)
+                {
+                    dr = dt.NewRow();
+                    dr[0] = i;
+                    dr[1] = pname;
+                    dr[2] = eamount;
+                    dt.Rows.Add(dr);
+                }
+                ds.Tables.Add(dt);
+                GridView1.DataSource = ds.Tables["emitable"].DefaultView;
+                GridView1.DataBind();
+            }
+
+
+
+
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
