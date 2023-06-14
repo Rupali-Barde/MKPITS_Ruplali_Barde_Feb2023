9b942f5d5231bf8734bdffe08e0a15e1eeaecacd
Rupali-Barde <rupalibarde311@gmail.com>
2023-06-14 10:38:20 +0530

    File Added

--- C Sharp/WebApplication6TRUST/Vendor.aspx
+++ C Sharp/WebApplication6TRUST/Vendor.aspx
@@ -5,11 +5,47 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+        .auto-style2 {
+            width: 100%;
+        }
+        .auto-style3 {
+            width: 896px;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
+        <div class="auto-style1">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Vendor_Mast<br />
+            <table class="auto-style2">
+                <tr>
+                    <td class="auto-style3">Vendor_Id</td>
+                    <td>
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style3">Vendor_Name</td>
+                    <td>&nbsp;<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+            </table>
         </div>
-    </form>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" />
+&nbsp;<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Update" />
+&nbsp;<asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Delete" />
+&nbsp;<asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Search" />
+&nbsp;<br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+&nbsp;</form>
 </body>
 </html>
--- C Sharp/WebApplication6TRUST/Vendor.aspx.cs
+++ C Sharp/WebApplication6TRUST/Vendor.aspx.cs
@@ -4,14 +4,91 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace WebApplication6TRUST
 {
     public partial class Vendor : System.Web.UI.Page
     {
+
+        SqlConnection con = new SqlConnection("server=.\\sqlexpress;integrated security=true;database=Trust");
+        DataSet ds = new DataSet();
+        SqlDataAdapter da;
+        string str = null;
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        public void loaddata()
+        {
+            str = "Select * from Vendor_Mast";
+            da = new SqlDataAdapter(str, con);
+            da.Fill(ds, "Vendor_Mast");
+            GridView1.DataSource = ds.Tables["Vendor_Mast"].DefaultView;
+            GridView1.DataBind();
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            str = "insert into Vendor_Mast values(@Vendor_Name)";
+            SqlCommand command = new SqlCommand(str, con);
+            // command.Parameters.AddWithValue("@Vendor_Id", Convert.ToInt32(TextBox1.Text));
+            command.Parameters.AddWithValue("@Vendor_Name", TextBox2.Text);
+            con.Open();
+            command.ExecuteNonQuery();
+            con.Close();
+            Label1.Text = "Record Inserted";
+            loaddata();
+            TextBox1.Text = "";
+            TextBox1.Focus();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            str = "update Vendor_Mast set Vendor_Name=@Vendor_Name where Vendor_Id=@Vendor_Id";
+            SqlCommand command = new SqlCommand(str, con);
+            command.Parameters.AddWithValue("@Vendor_Id", Convert.ToInt32(TextBox1.Text));
+            command.Parameters.AddWithValue("@Vendor_Name", TextBox2.Text);
+            con.Open();
+            command.ExecuteNonQuery();
+            con.Close();
+            Label1.Text = "Record Updated";
+            loaddata();
+            TextBox1.Text = "";
+            TextBox1.Focus();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            str = "delete from Vendor_Mast where Vendor_Id=@Vendor_Id";
+            SqlCommand command = new SqlCommand(str, con);
+
+            command.Parameters.AddWithValue("@Vendor_Id", Convert.ToInt32(TextBox1.Text));
+
+            con.Open();
+            command.ExecuteNonQuery();
+            con.Close();
+            Label1.Text = "record deleted ";
+            TextBox1.Text = "";
+            TextBox1.Focus();
+            loaddata();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            str = "select * from Vendor_Mast where Vendor_Id=@Vendor_Id";
+            da = new SqlDataAdapter(str, con);
+            da.SelectCommand.Parameters.AddWithValue("@Vendor_Id", Convert.ToInt32(TextBox1.Text));
+            da.Fill(ds, "Vendor_Mast");
+
+            TextBox2.Text = ds.Tables["Vendor_Mast"].Rows[0].ItemArray[1].ToString();
+            GridView1.DataSource = ds.Tables["Vendor_Mast"].DefaultView;
+
+            GridView1.DataBind();
         }
     }
 }
