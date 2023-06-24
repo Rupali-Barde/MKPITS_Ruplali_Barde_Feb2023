8b8c7998a850d47f3cbc6af8edc829dabd8854ff
Rupali-Barde <rupalibarde311@gmail.com>
2023-06-24 17:59:01 +0530

    File Added

---
+++ MVC/WebApplication6/WebApplication6/Controllers/ConditionalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class ConditionalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
